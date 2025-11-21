8fbf508636517508205ad76cf428a19ba2487736
caden234 <cputthoff03@gmail.com>
2025-11-20 21:57:10 -0600

    feat: display the data now in the browser

--- src/main.js
+++ src/main.js
@@ -11,3 +11,23 @@ form.addEventListener("submit", (event) => {
 
   console.log(data);
 });
+
+const result = document.querySelector("#result");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  // Pass the data object to the Results function to get HTML! 🚀
+  result.innerHTML = Results(data);
+});
+
+function Results(data) {
+  return `<ul class="space-y-1 text-sm">
+        ${Object.entries(data)
+          .map(([k, v]) => `<li><strong>${k}:</strong> ${v}</li>`)
+          .join("")}
+    </ul>   
+    `;
+}
