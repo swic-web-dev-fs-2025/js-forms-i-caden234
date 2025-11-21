91429f3d355f271b70a06bb5e11b91b200a7d5c7
caden234 <cputthoff03@gmail.com>
2025-11-20 21:41:06 -0600

    feat: create formdata abd log in console

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+const form = document.querySelector("form");
+
+// Same pattern as 'clicks', but for 'submit' events.
+form.addEventListener("submit", (event) => {
+  // No, browser! WE will handle this. Stop your default behavior.
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  console.log(data);
+});
